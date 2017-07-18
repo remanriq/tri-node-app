615c88c53c76f02c9987b6118cd16a053d59b8f7
Rosa Manriquez <rosamanriquez@Rosas-MacBook-Pro.local>
2017-07-17 18:28:57 -0700

    push this

--- liri.js
+++ liri.js
@@ -1,53 +1,93 @@
 
-
+var fs = require("fs");
 var request = require("request");
 var keys = require("./keys.js");
 var twitter = require("twitter");
 var spotify = require("spotify");
 var liriArgument = process.argv[2];
 
+console.log(process.argv);
 
-Switch(liriArgument){
-	case "movie-this":
-	movieThis();
-	break;
 
-	case "my-tweets":
-	myTweets();
-	break;
+ switch(liriArgument){
 
-	case "spotify-this-song":
-	songSearch();
-	break;
+case "movie-this":
+movieThis();
+break;
 
-	case "do-what-it-says":
-	doWhatitSays;
-	break;
+case "my-tweets":
+myTweets();
+break;
 
+case "spotify-this-song":
+songSearch();
+break;
+
+case "do-what-it-says":
+doWhatitSays;
+break;
+
+default: 
+	console.log("invalid argument");
 };
-
-
+	
 function movieThis(){
-var movieThis = process.argv[3];
-if(!movie){
-	movie = "";
-}
-params = movie
+var movieName = process.argv[3];
+
+if(movieName = pr
+
+
+console.log(movieName);
 
 // Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + The Matrix + "&y=&plot=short&apikey=40e9cece";
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+// This line is just to help us debug against the actual URL.
 console.log(queryUrl);
 
-request(queryUrl, function(error, response, body){
+request(queryUrl, function(error, response, body) { 
+	console.log(JSON.parse(body, null, 2));
 
   // If the request is successful
-if (!error && response.statusCode === 200) {
+  if (!error && response.statusCode === 200) {
 
     // Parse the body of the site and recover just the imdbRating
     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
     console.log("Release Year: " + JSON.parse(body).Year);
-}
+  }
+
+});
+
 
 };
-});
+ 
+ 
+
+// function myTweets(){
+
+
+// var myTweets = new twitter;
+
+// for (var i = 2; i < liriArgument.length; i++) {
+// 	if 
+// }
+// };
+
+// function spotifyThis(){
+
+
+
+// var spotify = new spotify-this-song;
+
+// for (var i = 2; i < liriArgument.length; i++){
+// 	if 
+// }
+// };
+
+
+
+
+ 
+
+
 
